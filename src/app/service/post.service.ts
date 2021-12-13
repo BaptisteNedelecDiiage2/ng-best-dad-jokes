198a4463bee067173332465fb299ecbf1ddd7a9d
Baptiste Nedelec <baptiste.nedelec@diiage.org>
2021-12-13 14:38:52 +0100

    feat(partie 1): Service

---
+++ src/app/service/post.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Post } from "../models/post.interface";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  constructor(private httpClient: HttpClient) { }
+
+  getAllPosts():Observable<Post[]> {
+    return this.httpClient.get<Post[]>{'http://localhost:3001/posts'};
+  }
+}
